2177109ac57c1d4fda9ce6b22f00403ff82bbba0
WheheoHu <wheheohu@outlook.com>
2024-02-05 12:06:34 +0800

    fix bug at __init__.py

--- dftt_timecode/__init__.py
+++ dftt_timecode/__init__.py
@@ -3,4 +3,4 @@ __author__ = 'You Ziyuan'
 __version__ = '0.0.12'
 
 from dftt_timecode.core.dftt_timecode import DfttTimecode
-from dftt_timecode.core.dftt_timecode import DfttTimeRange
+from dftt_timecode.core.dtff_timerange import DfttTimeRange
